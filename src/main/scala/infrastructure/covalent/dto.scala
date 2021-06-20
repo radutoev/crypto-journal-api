766bfc5bef0a59a80808b49c7e1ace00e6be4c45
radutoev <radu.toev@softwarechain.io>
2021-06-20 16:02:21 +0300

    Extend dto model for covalent api

--- src/main/scala/infrastructure/covalent/dto.scala
+++ src/main/scala/infrastructure/covalent/dto.scala
@@ -3,18 +3,32 @@ package infrastructure.covalent
 
 import domain.blockchain.Transaction
 
-import zio.json.{ DeriveJsonCodec, JsonCodec }
+import zio.json.{DeriveJsonDecoder, JsonDecoder, jsonField}
 
 object dto {
-  final case class TransactionQueryResponse(data: TransactionResponse)
+  final case class TransactionQueryResponse(
+    data: TransactionResponse,
+    error: Boolean,
+    @jsonField("error_code") errorCode: Int
+  )
 
   object TransactionQueryResponse {
-    implicit val encoder: JsonCodec[TransactionQueryResponse] = DeriveJsonCodec.gen[TransactionQueryResponse]
+    implicit val encoder: JsonDecoder[TransactionQueryResponse] = DeriveJsonDecoder.gen[TransactionQueryResponse]
   }
 
   final case class TransactionResponse(quote_currency: Option[String], items: List[Transaction])
 
   object TransactionResponse {
-    implicit val encoder: JsonCodec[TransactionResponse] = DeriveJsonCodec.gen[TransactionResponse]
+    implicit val encoder: JsonDecoder[TransactionResponse] = DeriveJsonDecoder.gen[TransactionResponse]
+  }
+
+  final case class Pagination(
+    @jsonField("has_more") hasMore: Boolean,
+    @jsonField("page_number") pageNumber: Int,
+    @jsonField("page_size") pageSize: Int
+  )
+
+  object Pagination {
+    implicit val encoder: JsonDecoder[Pagination] = DeriveJsonDecoder.gen[Pagination]
   }
 }
