0cf766685cf321da2b3c42bf9d15485f77d52088
radutoev <radu.toev@softwarechain.io>
2021-08-01 12:13:15 +0300

    Apply formatting

--- src/main/scala/domain/portfolio/PortfolioKpi.scala
+++ src/main/scala/domain/portfolio/PortfolioKpi.scala
@@ -65,14 +65,11 @@ final class PortfolioKpi(positions: Positions, interval: TimeInterval) {
     trend(_.fiatReturn())
   }
 
-  private def trend(of: Position => Option[FungibleData]): List[FungibleData] = {
+  private def trend(of: Position => Option[FungibleData]): List[FungibleData] =
     positions.items.headOption.map(_.openedAt).fold[List[FungibleData]](List.empty) { openedAt =>
       val interval = TimeInterval(openedAt.atBeginningOfDay(), Instant.now()) //should be an implicit
-      interval.days().map { day =>
-        positions.filter(TimeInterval(interval.start, day)).items.map(of).sumFungibleData()
-      }
+      interval.days().map(day => positions.filter(TimeInterval(interval.start, day)).items.map(of).sumFungibleData())
     }
-  }
 
   /**
    * Sum all fees of positions
