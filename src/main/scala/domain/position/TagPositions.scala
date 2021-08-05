3913d0f6f889067ba3c4dfe6cdcca24953e1b37f
radutoev <radu.toev@softwarechain.io>
2021-08-05 11:41:01 +0300

    Remove unused case class

--- src/main/scala/domain/position/TagPositions.scala
+++
@@ -1,6 +0,0 @@
-package io.softwarechain.cryptojournal
-package domain.position
-
-import domain.position.Position.PositionId
-
-final case class TagPositions(tags: List[String], ids: List[PositionId])
