93df025fbc9df065391b3393575fb158da1b7b42
radutoev <radu.toev@softwarechain.io>
2021-10-09 09:15:05 +0300

    Remove wallet events

--- src/shared/main/scala/domain/wallet/event.scala
+++
@@ -1,8 +0,0 @@
-package io.softwarechain.cryptojournal
-package domain.wallet
-
-object event {
-  sealed trait WalletEvent
-  final case class WalletAdded(wallet: Wallet)   extends WalletEvent
-  final case class WalletRemoved(wallet: Wallet) extends WalletEvent
-}
