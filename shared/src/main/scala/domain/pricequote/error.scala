b64969c52b537067f6129d7c2df20f5d74e8a0c3
radutoev <radu.toev@softwarechain.io>
2021-11-19 11:59:32 +0200

    Move price quote functionality to shared module

--- shared/src/main/scala/domain/pricequote/error.scala
+++ shared/src/main/scala/domain/pricequote/error.scala
@@ -5,6 +5,6 @@ import domain.model.CoinAddress
 
 object error {
   sealed trait PriceQuoteError
-  final case class PriceQuoteFetchError(message: String) extends PriceQuoteError
+  final case class PriceQuoteFetchError(message: String)     extends PriceQuoteError
   final case class PriceQuoteNotFound(contract: CoinAddress) extends PriceQuoteError
 }
