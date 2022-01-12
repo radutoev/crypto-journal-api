da8238a3b730b1dcd24bab6756ea69fff4e42ece
radutoev <radu.toev@softwarechain.io>
2022-01-12 17:44:42 +0200

    Split keys in chunks for datastore get ops

--- shared/src/main/scala/infrastructure/google/datastore/DatastoreOps.scala
+++ shared/src/main/scala/infrastructure/google/datastore/DatastoreOps.scala
@@ -2,7 +2,7 @@ package io.softwarechain.cryptojournal
 package infrastructure.google.datastore
 
 import com.google.cloud.datastore.{Datastore, Entity, Key, Query, QueryResults, ReadOption}
-import zio.Task
+import zio.{IO, Task, ZIO}
 import zio.logging.Logger
 
 import scala.jdk.CollectionConverters.{IterableHasAsJava, IteratorHasAsScala}
@@ -17,8 +17,21 @@ trait DatastoreOps {
       .tapError(throwable => logger.warn(throwable.getMessage))
       .map(Option(_))
 
-  def get(keys: Set[Key])(implicit datastore: Datastore, logger: Logger[String]): Task[List[Entity]] =
-    Task(datastore.get(keys.asJava))
-      .tapError(throwable => logger.warn(throwable.getMessage))
-      .map(_.asScala.toList)
+  /**
+   * Datastore has a limit of 1000 keys that can be fetched.
+   *  -> "cannot get more than 1000 keys in a single call" exception.
+   */
+  def get(keys: Set[Key])(implicit datastore: Datastore, logger: Logger[String]): Task[List[Entity]] = {
+    def doGet(batch: Set[Key]): IO[Option[String], List[Entity]] = {
+      Task(datastore.get(batch.asJava))
+        .tapError(throwable => logger.warn(throwable.getMessage))
+        .mapBoth(_ => Some(s"Unable to fetch data for $batch"), _.asScala.toList)
+    }
+
+    ZIO.collect(keys.grouped(1000).toList)(doGet)
+      .mapBoth(
+        _ => new RuntimeException(s"Unable to fetch data for $keys"),
+        _.flatten.toList
+      )
+  }
 }
